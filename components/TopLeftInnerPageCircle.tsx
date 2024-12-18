a850b3c5fbd206c738aa8232c047905d11dc2d3d
Minh Pham <minh.pham@fastco.asia>
2024-12-19 03:02:48 +0700

    first commit

---
+++ components/TopLeftInnerPageCircle.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function TopLeftInnerPageCircle() {
+  return (
+    <div>
+      <div className=" tradex-z-[-1] tradex-top-0 tradex-absolute tradex-left-0 tradex-w-[335px] tradex-h-[335px] tradex-rounded-full tradex-bg-primary/30 tradex-blur-[140px]" />
+      <img
+        src="/inner_banner_top_left_circle.png"
+        className=" tradex-absolute tradex-top-[30px] tradex-left-[40px]"
+        alt=""
+      />
+      <img
+        src="/inner_banner_top_left_small_circle.png"
+        className="tradex-z-[-1] tradex-absolute tradex-top-[212px] tradex-left-[100px]"
+        alt=""
+      />
+    </div>
+  );
+}
---
+++ components/TopRightInnerPageCircle.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function TopRightInnerPageCircle() {
+  return (
+    <div>
+      <div className=" tradex-z-[-1] tradex-top-0 tradex-absolute tradex-right-0 tradex-w-[335px] tradex-h-[335px] tradex-rounded-full tradex-bg-primary/30 tradex-blur-[140px]" />
+      <img
+        src="/inner_banner_top_right.png"
+        className="tradex-z-[-1] tradex-absolute tradex-top-0 tradex-right-0"
+        alt=""
+      />
+      <img
+        src="/inner_banner_top_left_circle.png"
+        className="tradex-z-[-1] tradex-absolute tradex-top-[65px] tradex-right-[50px]"
+        alt=""
+      />
+      <img
+        src="/inner_banner_top_left_small_circle.png"
+        className="tradex-z-[-1] tradex-absolute tradex-top-[235px] tradex-right-[105px]"
+        alt=""
+      />
+    </div>
+  );
+}
